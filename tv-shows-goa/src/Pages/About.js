34cf35eb9a4a2b84898fbb5c1d47badcb355ae9b
Nicolas Fuchs <nicolasf10>
2021-04-28 14:58:30 -0300

    git pages

--- tv-shows-goa/src/Pages/About.js
+++ tv-shows-goa/src/Pages/About.js
@@ -25,7 +25,7 @@ function About() {
             <Container>
                 <h1>About the Project</h1>
                 <p>
-                    Hello! My name is <span style={{fontWeight: 800}}>Nicolas Fuchs</span>, and this is my GOA final project (<span style={{fontStyle: 'italic'}}>CSII: ANALYZING DATA WITH PYTHON</span>). On this website, you’ll be able to see two main pages containing data visualizations for the popularity and location of Tweets related to multiple TV Shows. The data displayed was gathered using Twitter’s API, and the visualizations with the Bokeh library. Visit the GitHub repository for this project here: <a href='https://github.com/nicolasf10/tv-shows' target="a_blank">https://github.com/nicolasf10/tv-shows</a>.
+                    Hello! My name is <span style={{fontWeight: 800}}>Nicolas Fuchs</span>, and this is my GOA final project (<span style={{fontStyle: 'italic'}}>CSII: ANALYZING DATA WITH PYTHON</span>). On this website, you’ll see two main pages containing data visualizations for the popularity and location of Tweets related to multiple TV Shows. The data displayed was gathered using Twitter’s API, and the visualizations with the Bokeh library. Visit the GitHub repository for this project here: <a href='https://github.com/nicolasf10/tv-shows' target="a_blank">https://github.com/nicolasf10/tv-shows</a>.
                     (Note: Some shows had to be removed as they didn’t have very unique names, so, otherwise, Tweets about other topics would be be considered in the data set.)
                 </p>
                 <h2>Visualizations</h2>
