c9237c35043d0c994ac66730d3b06b2262592dec
Nicolas Fuchs <nicolasf10>
2021-04-28 14:46:12 -0300

    Alert added

--- tv-shows-goa/src/App.js
+++ tv-shows-goa/src/App.js
@@ -3,6 +3,7 @@ import Home from './Pages/Home';
 import About from './Pages/About';
 import Nav from './Components/Nav';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Alert';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,6 +12,10 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
+        <Alert variant="info" onClose={() => setShow(false)} dismissible>
+            If the links to the data visualizations don't work, go to the README in the {' '}
+            <Alert.Link href="https://github.com/nicolasf10/tv-shows">GitHub repository</Alert.Link>.
+          </Alert>
         <Switch>
           <Route path='/tv-shows/' exact component={Home}/>
           <Route path='/tv-shows/about' exact component={About}/>
